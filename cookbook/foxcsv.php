3cb74c67f98b84d435891c7595ea872b17e1486e
Pierre Racine <pierre.racine@sbf.ulaval.ca>
2024-12-05 16:03:08 -0500

    More port to PHP 8

--- cookbook/foxcsv.php
+++ cookbook/foxcsv.php
@@ -328,7 +328,8 @@ function fxc_Parse_CSV( $text, $sep ) {
     # show($row,'row adjusted');
         $data[$i] = $row;
     }
-    $data[0] = fxc_Normalise_Header($data[0]);
+    if (isset($data[0]))
+      $data[0] = fxc_Normalise_Header($data[0]);
  #show($data,'data');
     return $data;
 } //}}}
